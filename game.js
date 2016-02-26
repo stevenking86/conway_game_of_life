22e3deb364014863ddfb3ed3069bfd5a21318066
Steven King <email": null>
2016-02-26 15:27:43 -0500

    write check_all_cells

--- game.js
+++ game.js
@@ -15,7 +15,18 @@ var grid = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
             [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
 ]
 
-var future_grid = []
+var future_grid = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+]
 
 function seed_board () {
     for (var i = 0; i < STARTING_LIFE; i ++) {
@@ -27,47 +38,68 @@ function print_board () {
   console.log(grid)
 }
 
+function print_future_board() {
+    console.log(future_grid)
+}
+
 
 
 
 function check_neighbors (grid,outer,inner) {
     var live_neighbor_count = 0
-    if (grid[outer - 1][inner-1] == 1){
+    if (grid[outer - 1][inner-1] === 1){
         live_neighbor_count += 1
     }
-    if (grid[outer - 1][inner] == 1){
+    if (grid[outer - 1][inner] === 1){
         live_neighbor_count += 1
     }
-    if (grid[outer - 1][inner+1] == 1){
+    if (grid[outer - 1][inner+1] === 1){
         live_neighbor_count += 1
     }
-    if (grid[outer][inner-1] == 1){
+    if (grid[outer][inner-1] === 1){
         live_neighbor_count += 1
     }
-    if (grid[outer][inner+1] == 1){
+    if (grid[outer][inner+1] === 1){
         live_neighbor_count += 1
     }
-    if (grid[outer + 1][inner-1] == 1){
+    if (grid[outer + 1][inner-1] === 1){
         live_neighbor_count += 1
     }
-    if (grid[outer + 1][inner] == 1){
+    if (grid[outer + 1][inner] === 1){
         live_neighbor_count += 1
     }
-    if (grid[outer + 1][inner+1] == 1){
+    if (grid[outer + 1][inner+1] === 1){
         live_neighbor_count += 1
     }
     return live_neighbor_count
 }
 
 function live_or_die(grid, outer, inner) {
-    if (check_neighbors(grid,[outer][inner]) >= 3) {
-       future_grid[outer][inner] == 1
+    if (check_neighbors(grid,outer,inner) >= 3) {
+       future_grid[outer][inner] = 1
+       console.log("LIVE")
     }
+    else {
+        future_grid[outer][inner] = 0
+        console.log("DEAD")
+    }
+    return future_grid
+}
+
+function check_all_cells (grid){
+    var new_grid
+    for (var i = 0; i < grid.length; i++) {
+        for (var j = 0; j < grid[i].length; j++){
+          new_grid = live_or_die(grid, i, j)
+        }
+    }
+  console.log(new_grid)
 }
 
 //DRIVER TEST CODE
 seed_board()
 print_board()
-console.log(check_neighbors(grid, 1,2))
-
-
+// console.log(check_neighbors(grid, 1,2))
+// live_or_die(grid,1,2)
+check_all_cells(grid)
+//console.log(future_grid)
